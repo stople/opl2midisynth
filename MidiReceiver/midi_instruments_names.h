8e5ae43b069830060e9b6222c13c704e882d6d2b
Eirik Stople <eirik@pcfood.net>
2019-01-13 17:53:14 +0100

    Move midi instrument names to separate file

---
+++ MidiReceiver/midi_instruments_names.h
@@ -0,0 +1,160 @@
+#ifndef MIDI_INSTRUMENT_NAMES_h
+#define MIDI_INSTRUMENT_NAMES_h
+
+//A const array of the instrument names as found in midi_instruments.h in ArduinoOpl2
+
+const char textPIANO1[] PROGMEM = "PIANO1";
+const char textPIANO2[] PROGMEM = "PIANO2";
+const char textPIANO3[] PROGMEM = "PIANO3";
+const char textHONKTONK[] PROGMEM = "HONKTONK";
+const char textEP1[] PROGMEM = "EP1";
+const char textEP2[] PROGMEM = "EP2";
+const char textHARPSIC[] PROGMEM = "HARPSIC";
+const char textCLAVIC[] PROGMEM = "CLAVIC";
+const char textCELESTA[] PROGMEM = "CELESTA";
+const char textGLOCK[] PROGMEM = "GLOCK";
+const char textMUSICBOX[] PROGMEM = "MUSICBOX";
+const char textVIBES[] PROGMEM = "VIBES";
+const char textMARIMBA[] PROGMEM = "MARIMBA";
+const char textXYLO[] PROGMEM = "XYLO";
+const char textTUBEBELL[] PROGMEM = "TUBEBELL";
+const char textSANTUR[] PROGMEM = "SANTUR";
+const char textORGAN1[] PROGMEM = "ORGAN1";
+const char textORGAN2[] PROGMEM = "ORGAN2";
+const char textORGAN3[] PROGMEM = "ORGAN3";
+const char textPIPEORG[] PROGMEM = "PIPEORG";
+const char textREEDORG[] PROGMEM = "REEDORG";
+const char textACORDIAN[] PROGMEM = "ACORDIAN";
+const char textHARMONIC[] PROGMEM = "HARMONIC";
+const char textBANDNEON[] PROGMEM = "BANDNEON";
+const char textNYLONGT[] PROGMEM = "NYLONGT";
+const char textSTEELGT[] PROGMEM = "STEELGT";
+const char textJAZZGT[] PROGMEM = "JAZZGT";
+const char textCLEANGT[] PROGMEM = "CLEANGT";
+const char textMUTEGT[] PROGMEM = "MUTEGT";
+const char textOVERDGT[] PROGMEM = "OVERDGT";
+const char textDISTGT[] PROGMEM = "DISTGT";
+const char textGTHARMS[] PROGMEM = "GTHARMS";
+const char textACOUBASS[] PROGMEM = "ACOUBASS";
+const char textFINGBASS[] PROGMEM = "FINGBASS";
+const char textPICKBASS[] PROGMEM = "PICKBASS";
+const char textFRETLESS[] PROGMEM = "FRETLESS";
+const char textSLAPBAS1[] PROGMEM = "SLAPBAS1";
+const char textSLAPBAS2[] PROGMEM = "SLAPBAS2";
+const char textSYNBASS1[] PROGMEM = "SYNBASS1";
+const char textSYNBASS2[] PROGMEM = "SYNBASS2";
+const char textVIOLIN[] PROGMEM = "VIOLIN";
+const char textVIOLA[] PROGMEM = "VIOLA";
+const char textCELLO[] PROGMEM = "CELLO";
+const char textCONTRAB[] PROGMEM = "CONTRAB";
+const char textTREMSTR[] PROGMEM = "TREMSTR";
+const char textPIZZ[] PROGMEM = "PIZZ";
+const char textHARP[] PROGMEM = "HARP";
+const char textTIMPANI[] PROGMEM = "TIMPANI";
+const char textSTRINGS[] PROGMEM = "STRINGS";
+const char textSLOWSTR[] PROGMEM = "SLOWSTR";
+const char textSYNSTR1[] PROGMEM = "SYNSTR1";
+const char textSYNSTR2[] PROGMEM = "SYNSTR2";
+const char textCHOIR[] PROGMEM = "CHOIR";
+const char textOOHS[] PROGMEM = "OOHS";
+const char textSYNVOX[] PROGMEM = "SYNVOX";
+const char textORCHIT[] PROGMEM = "ORCHIT";
+const char textTRUMPET[] PROGMEM = "TRUMPET";
+const char textTROMBONE[] PROGMEM = "TROMBONE";
+const char textTUBA[] PROGMEM = "TUBA";
+const char textMUTETRP[] PROGMEM = "MUTETRP";
+const char textFRHORN[] PROGMEM = "FRHORN";
+const char textBRASS1[] PROGMEM = "BRASS1";
+const char textSYNBRAS1[] PROGMEM = "SYNBRAS1";
+const char textSYNBRAS2[] PROGMEM = "SYNBRAS2";
+const char textSOPSAX[] PROGMEM = "SOPSAX";
+const char textALTOSAX[] PROGMEM = "ALTOSAX";
+const char textTENSAX[] PROGMEM = "TENSAX";
+const char textBARISAX[] PROGMEM = "BARISAX";
+const char textOBOE[] PROGMEM = "OBOE";
+const char textENGLHORN[] PROGMEM = "ENGLHORN";
+const char textBASSOON[] PROGMEM = "BASSOON";
+const char textCLARINET[] PROGMEM = "CLARINET";
+const char textPICCOLO[] PROGMEM = "PICCOLO";
+const char textFLUTE1[] PROGMEM = "FLUTE1";
+const char textRECORDER[] PROGMEM = "RECORDER";
+const char textPANFLUTE[] PROGMEM = "PANFLUTE";
+const char textBOTTLEB[] PROGMEM = "BOTTLEB";
+const char textSHAKU[] PROGMEM = "SHAKU";
+const char textWHISTLE[] PROGMEM = "WHISTLE";
+const char textOCARINA[] PROGMEM = "OCARINA";
+const char textSQUARWAV[] PROGMEM = "SQUARWAV";
+const char textSAWWAV[] PROGMEM = "SAWWAV";
+const char textSYNCALLI[] PROGMEM = "SYNCALLI";
+const char textCHIFLEAD[] PROGMEM = "CHIFLEAD";
+const char textCHARANG[] PROGMEM = "CHARANG";
+const char textSOLOVOX[] PROGMEM = "SOLOVOX";
+const char textFIFTHSAW[] PROGMEM = "FIFTHSAW";
+const char textBASSLEAD[] PROGMEM = "BASSLEAD";
+const char textFANTASIA[] PROGMEM = "FANTASIA";
+const char textWARMPAD[] PROGMEM = "WARMPAD";
+const char textPOLYSYN[] PROGMEM = "POLYSYN";
+const char textSPACEVOX[] PROGMEM = "SPACEVOX";
+const char textBOWEDGLS[] PROGMEM = "BOWEDGLS";
+const char textMETALPAD[] PROGMEM = "METALPAD";
+const char textHALOPAD[] PROGMEM = "HALOPAD";
+const char textSWEEPPAD[] PROGMEM = "SWEEPPAD";
+const char textICERAIN[] PROGMEM = "ICERAIN";
+const char textSOUNDTRK[] PROGMEM = "SOUNDTRK";
+const char textCRYSTAL[] PROGMEM = "CRYSTAL";
+const char textATMOSPH[] PROGMEM = "ATMOSPH";
+const char textBRIGHT[] PROGMEM = "BRIGHT";
+const char textGOBLIN[] PROGMEM = "GOBLIN";
+const char textECHODROP[] PROGMEM = "ECHODROP";
+const char textSTARTHEM[] PROGMEM = "STARTHEM";
+const char textSITAR[] PROGMEM = "SITAR";
+const char textBANJO[] PROGMEM = "BANJO";
+const char textSHAMISEN[] PROGMEM = "SHAMISEN";
+const char textKOTO[] PROGMEM = "KOTO";
+const char textKALIMBA[] PROGMEM = "KALIMBA";
+const char textBAGPIPE[] PROGMEM = "BAGPIPE";
+const char textFIDDLE[] PROGMEM = "FIDDLE";
+const char textSHANNAI[] PROGMEM = "SHANNAI";
+const char textTINKLBEL[] PROGMEM = "TINKLBEL";
+const char textAGOGO[] PROGMEM = "AGOGO";
+const char textSTEELDRM[] PROGMEM = "STEELDRM";
+const char textWOODBLOK[] PROGMEM = "WOODBLOK";
+const char textTAIKO[] PROGMEM = "TAIKO";
+const char textMELOTOM[] PROGMEM = "MELOTOM";
+const char textSYNDRUM[] PROGMEM = "SYNDRUM";
+const char textREVRSCYM[] PROGMEM = "REVRSCYM";
+const char textFRETNOIS[] PROGMEM = "FRETNOIS";
+const char textBRTHNOIS[] PROGMEM = "BRTHNOIS";
+const char textSEASHORE[] PROGMEM = "SEASHORE";
+const char textBIRDS[] PROGMEM = "BIRDS";
+const char textTELEPHON[] PROGMEM = "TELEPHON";
+const char textHELICOPT[] PROGMEM = "HELICOPT";
+const char textAPPLAUSE[] PROGMEM = "APPLAUSE";
+const char textGUNSHOT[] PROGMEM = "GUNSHOT";
+
+const char* const midiInstrumentNames[] PROGMEM = {
+  textPIANO1,   textPIANO2,   textPIANO3,   textHONKTONK, textEP1,      textEP2,
+  textHARPSIC,  textCLAVIC,   textCELESTA,  textGLOCK,    textMUSICBOX, textVIBES,
+  textMARIMBA,  textXYLO,     textTUBEBELL, textSANTUR,   textORGAN1,   textORGAN2,
+  textORGAN3,   textPIPEORG,  textREEDORG,  textACORDIAN, textHARMONIC, textBANDNEON,
+  textNYLONGT,  textSTEELGT,  textJAZZGT,   textCLEANGT,  textMUTEGT,   textOVERDGT,
+  textDISTGT,   textGTHARMS,  textACOUBASS, textFINGBASS, textPICKBASS, textFRETLESS,
+  textSLAPBAS1, textSLAPBAS2, textSYNBASS1, textSYNBASS2, textVIOLIN,   textVIOLA,
+  textCELLO,    textCONTRAB,  textTREMSTR,  textPIZZ,     textHARP,     textTIMPANI,
+  textSTRINGS,  textSLOWSTR,  textSYNSTR1,  textSYNSTR2,  textCHOIR,    textOOHS,
+  textSYNVOX,   textORCHIT,   textTRUMPET,  textTROMBONE, textTUBA,     textMUTETRP,
+  textFRHORN,   textBRASS1,   textSYNBRAS1, textSYNBRAS2, textSOPSAX,   textALTOSAX,
+  textTENSAX,   textBARISAX,  textOBOE,     textENGLHORN, textBASSOON,  textCLARINET,
+  textPICCOLO,  textFLUTE1,   textRECORDER, textPANFLUTE, textBOTTLEB,  textSHAKU,
+  textWHISTLE,  textOCARINA,  textSQUARWAV, textSAWWAV,   textSYNCALLI, textCHIFLEAD,
+  textCHARANG,  textSOLOVOX,  textFIFTHSAW, textBASSLEAD, textFANTASIA, textWARMPAD,
+  textPOLYSYN,  textSPACEVOX, textBOWEDGLS, textMETALPAD, textHALOPAD,  textSWEEPPAD,
+  textICERAIN,  textSOUNDTRK, textCRYSTAL,  textATMOSPH,  textBRIGHT,   textGOBLIN,
+  textECHODROP, textSTARTHEM, textSITAR,    textBANJO,    textSHAMISEN, textKOTO,
+  textKALIMBA,  textBAGPIPE,  textFIDDLE,   textSHANNAI,  textTINKLBEL, textAGOGO,
+  textSTEELDRM, textWOODBLOK, textTAIKO,    textMELOTOM,  textSYNDRUM,  textREVRSCYM,
+  textFRETNOIS, textBRTHNOIS, textSEASHORE, textBIRDS,    textTELEPHON, textHELICOPT,
+  textAPPLAUSE, textGUNSHOT
+};
+
+#endif
